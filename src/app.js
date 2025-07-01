5fddda22146e3f9622bd415843427d93bcf8e2eb
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-01 05:00:15 +0200

    Create a default todo list project

--- src/app.js
+++ src/app.js
@@ -1,20 +1,31 @@
 const app = (() => {
-  const todoList = [];
+  const todoList = [{
+    projectTitle: 'Getting Started',
+    projectId: crypto.randomUUID(),
+    projectTodo: [{
+      todoTitle: 'Add your first real task',
+      todoDueDate: new Date().toISOString().split('T')[0],
+      todoPriority: 'Medium',
+      todoDescription: 'Let\'s get started!',
+      todoCheck: false,
+      todoId: crypto.randomUUID()
+    }]
+  }];
   const createProject = (title) => {
     todoList.push({
-      title,
-      id: crypto.randomUUID(),
-      todo: []
+      projectTitle: title,
+      projectId: crypto.randomUUID(),
+      projectTodo: []
     })
   }
 
   const createTodo = (index, title, dueDate, priority, description) => {
-    todoList[index].todo.push({
-      title,
-      dueDate,
-      priority,
-      description,
-      check: false,
+    todoList[index].projectTodo.push({
+      todoTitle: title,
+      todoDueDate: dueDate,
+      todoPriority: priority,
+      todoDescription: description,
+      todoCheck: false,
       todoId: crypto.randomUUID()
     });
   };
